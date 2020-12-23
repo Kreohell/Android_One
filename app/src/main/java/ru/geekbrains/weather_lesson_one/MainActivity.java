91fb49fb43751b85e064ddd73e25b5f31bdeede5
Kreohell <kreohell@rambler.ru>
2020-12-23 22:57:32 +0300

    add homework two

--- app/src/main/java/ru/geekbrains/weather_lesson_one/MainActivity.java
+++ app/src/main/java/ru/geekbrains/weather_lesson_one/MainActivity.java
@@ -3,12 +3,19 @@ package ru.geekbrains.weather_lesson_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] cities = {"City_1", "City_2", "City_3", "City_4", "City_5"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, cities);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
